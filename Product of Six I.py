c0b791bfe9b49808e2fd29088f72e892eb58cbef
gouthamgoutham99 <123544909+gouthamgoutham99@users.noreply.github.com>
2026-05-09 18:29:53 +0530

    Add program to calculate product of six numbers

---
+++ Product of Six I.py
@@ -0,0 +1,16 @@
+one = int(input("Enter first number: "))
+two = int(input("Enter second number: "))
+three = int(input("Enter third number: "))
+four = int(input("Enter fourth number: "))
+five = int(input("Enter fifth number: "))
+six = int(input("Enter sixth number: "))
+
+product = 1
+product = product * one
+product = product * two
+product = product * three
+product = product * four
+product = product * five
+product = product * six
+
+print("The product is:", product)
